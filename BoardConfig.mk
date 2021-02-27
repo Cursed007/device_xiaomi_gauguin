22980ff9e029ec71227e05375ed0cd294184e664
Martin <recordyletsplays@gmail.com>
2021-02-27 01:04:45 +0100

    gauguin: Generate APEX image

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,6 +9,9 @@ DEVICE_PATH := device/xiaomi/gauguin
 
 BUILD_BROKEN_DUP_RULES := true
 
+# APEX
+DEXPREOPT_GENERATE_APEX_IMAGE := true
+
 # Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
