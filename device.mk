8ba883840f307623b7cb236255d42d46074e78cc
Cursed007 <huha.lula@gmail.com>
2021-06-29 07:29:28 +0000

    gauguin: removed nfc, defined a missing sepolicy, power_profile nerfed

--- device.mk
+++ device.mk
@@ -115,12 +115,12 @@ PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.xiaomi_gauguin
 
 # NFC
-PRODUCT_PACKAGES += \
-    com.android.nfc_extras \
-    com.gsma.services.nfc \
-    NfcNci \
-    SecureElement \
-    Tag
+# PRODUCT_PACKAGES += \
+#     com.android.nfc_extras \
+#     com.gsma.services.nfc \
+#     NfcNci \
+#     SecureElement \
+#     Tag
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
