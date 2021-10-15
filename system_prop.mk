255ff7c8529f1aa8a484de7baebcef6faa3bcc55
Cursed007 <huha.lula@gmail.com>
2021-10-15 16:14:39 +0200

    Revert "gauguin: Enable VoLTE/VoWiFi for all carrier"
    This reverts commit add86193e6bd02a785896bd16a4d63c9be3237be.

--- system_prop.mk
+++ system_prop.mk
@@ -45,9 +45,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # RIL
 PRODUCT_PRODUCTY_OVERRIDES += \
-    ro.telephony.default_network=33,22 \
-    persist.dbg.volte_avail_ovr=1 \
-    persist.dbg.wfc_avail_ovr=1
+    ro.telephony.default_network=33,22
 
 # Vulkan
 PRODUCT_PROPERTY_OVERRIDES += \
